6bfca30a4b26786e3d49dc0c56c9e4803c2e0c88
trunglvb <levubaotrung0311@gmail.com>
2024-11-17 21:38:51 +0700

    setup google api

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,6 +1,19 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+	const getGoolgeAuthUrl = () => {
+		const { VITE_GOOGLE_CLIENT_ID, VITE_REDIRECT_URI } = import.meta.env;
+		const url = "https://accounts.google.com/o/oauth2/v2/auth";
+		const query = {
+			client_id: VITE_GOOGLE_CLIENT_ID,
+			redirect_uri: VITE_REDIRECT_URI,
+			response_type: "code",
+			scope: [
+				"https://www.googleapis.com/auth/userinfo.profile",
+				"https://www.googleapis.com/auth/userinfo.email",
+			].join(" "),
+		};
+	};
 	return (
 		<div>
 			<Button>Login Google</Button>
