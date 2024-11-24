302deffb0263234d43c67c828480df3241d71616
trunglvb <levubaotrung0311@gmail.com>
2024-11-24 22:46:13 +0700

    handle upload single image

--- server/src/utils/file.ts
+++ server/src/utils/file.ts
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import path from 'path';
+import { Request } from 'express';
+import formidable from 'formidable';
 
 export const initFolder = () => {
   const uploadFolderPath = path.resolve('uploads');
@@ -8,4 +10,22 @@ export const initFolder = () => {
       recursive: true // tao folder nested
     });
   }
+};
+
+export const handleUploadSingleImage = async (req: Request) => {
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    maxFiles: 1,
+    keepExtensions: true, // khi upload thi lay duoi mo rong,
+    maxFileSize: 300 * 1024 // 300kb
+  });
+
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(files);
+    });
+  });
 };
