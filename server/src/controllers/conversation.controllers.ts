a68892805d2de02c028071a39d2cca02477c05ba
trunglvb <levubaotrung0311@gmail.com>
2025-04-16 22:41:07 +0700

    route get conversations

--- server/src/controllers/conversation.controllers.ts
+++ server/src/controllers/conversation.controllers.ts
@@ -6,12 +6,26 @@ import { NextFunction, Request, Response } from 'express';
 const getConversationController = async (req: Request, res: Response, _next: NextFunction) => {
   const sender_id = req.decode_access_token?.user_id! as string;
   const { receiver_id } = req.params as IGetConversationParams;
+  const { limit = 10, page = 1 } = req.query;
 
-  const conversations = await conversationService.getConversation(sender_id, receiver_id);
+  const conversations = await conversationService.getConversation({
+    senderId: sender_id,
+    receiverId: receiver_id,
+    limit: Number(limit),
+    page: Number(page)
+  });
+
+  console.log(conversations);
 
   return res.status(HttpStatusCode.Ok).json({
-    message: 'Get user infomation success',
-    result: conversations
+    message: 'Get conversations success',
+    result: {
+      conversations: conversations.conversation,
+      total: conversations.total,
+      limit: limit,
+      page: page,
+      totalPage: Math.ceil(Number(conversations.total) / Number(limit))
+    }
   });
 };
 
--- server/src/services/conversation.services.ts
+++ server/src/services/conversation.services.ts
@@ -2,17 +2,28 @@ import databaseService from '@/services/database.services';
 import { de_AT } from '@faker-js/faker';
 import { ObjectId } from 'mongodb';
 
+interface IGetConversationParams {
+  senderId: string;
+  receiverId: string;
+  limit: number;
+  page: number;
+}
 class ConversationService {
-  getConversation = async (senderId: string, receiverId: string) => {
-    console.log(senderId, receiverId);
+  getConversation = async (params: IGetConversationParams) => {
+    const { senderId, receiverId, limit, page } = params;
+    const match = {
+      $or: [
+        { sender_id: new ObjectId(senderId), receiver_id: new ObjectId(receiverId) },
+        { sender_id: new ObjectId(receiverId), receiver_id: new ObjectId(senderId) }
+      ]
+    };
     const conversation = await databaseService.conversation
-      .find({
-        sender_id: new ObjectId(senderId),
-        receiver_id: new ObjectId(receiverId)
-      })
+      .find(match)
+      .skip((page - 1) * limit)
+      .limit(limit)
       .toArray();
-    console.log('conversation', conversation);
-    return conversation;
+    const total = await databaseService.conversation.countDocuments(match);
+    return { conversation, total };
   };
 }
 
